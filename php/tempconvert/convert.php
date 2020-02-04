9901fd45d6e4babbf5c29b9a114e7589cd145295
Patryk Sipowicz <patryksipowicz@gmail.com>
2020-02-04 12:25:29 +0000

    converter of temperature; testing connection

---
+++ php/tempconvert/convert.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Temperature Conversion Page</title>
+ </head>
+ <body>
+ <h1>Temperature Conversion Page</h1>
+ 
+<?php
+$c = $_GET['temp'];
+$c=intval($c);
+$f = ($c * 9/5) + 32;
+echo ($c . " degrees Celsius is " . $f . " degrees Fahrenheit");
+ ?>
+ 
+ </body>
+</html>
