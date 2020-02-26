76ff1db710aa35d5957fa06c50ad9a8b1de5b9b7
Patryk Sipowicz <patryksipowicz@gmail.com>
2020-02-26 09:55:59 +0000

    Events JS

--- php/welcome.php
+++ php/welcome.php
@@ -9,6 +9,11 @@
 
    <body>
       <h1>Welcome <?php echo $login_session; ?></h1>
+      <p id = "Events">I will display your events here.</p>
+      <button type = "button"
+      onclick='document.getElementById("Events").innerHTML =
+      "Your events are: "'>Click to display events!
+      </button>
       <h2><a href = "logout.php">Sign Out</a></h2>
    </body>
 
