5b385f51c604cd0e10ab9fdc31463bafea09df57
k55592hr <horia.radu@student.manchester.ac.uk>
2020-03-13 22:37:45 +0000

    testing

--- timetableLogic/moreTable.php
+++ timetableLogic/moreTable.php
@@ -143,7 +143,7 @@
                         }
 
                       foreach($listOfCourses as $ids) {
-                        $sqlQuery2 = "SELECT startTime, name FROM CourseEvents WHERE name = " . $ids[0] . " AND lab = " . $ids[1];
+                        $sqlQuery2 = ("SELECT startTime, name FROM CourseEvents WHERE name = " . $ids[0] . " AND lab = " . $ids[1]);
                         $fetchedEvent2 = $conn->query($sqlQuery2);
                         foreach($fetchedEvent2->fetch_all(MYSQLI_ASSOC) as $row) {
                           $timeTillEvent = hours_between($monday, $row["startTime"]);
