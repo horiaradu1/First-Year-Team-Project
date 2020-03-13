d18d49c28a6d30e3f899dda2364d42bb118aae19
k55592hr <horia.radu@student.manchester.ac.uk>
2020-03-13 23:15:36 +0000

    testing

--- timetableLogic/moreTable.php
+++ timetableLogic/moreTable.php
@@ -111,7 +111,7 @@ error_reporting(E_ERROR);
                       $event = NULL;
                       $listOfEventIDs = array();
                       $listOfCourses = array();
-                      $listOfLabs = array();
+                      $colour = "column100 column2";
 
                       $username = "horia"; // CHANGE USERNAME BASED ON WHO IS LOGGED IN
 
@@ -132,6 +132,7 @@ error_reporting(E_ERROR);
 
                           if ($timeTillEventHours == $i && $timeTillEventDays == $j ){
                             $event = "$event AND " . $row["name"];
+                            $colour = "column100 blue";
                             }
                           }
                         }
@@ -152,7 +153,7 @@ error_reporting(E_ERROR);
                           $timeTillEventDays = (int)$timeTillEventDays;
 
                           if ($timeTillEventHours == $i && $timeTillEventDays == $j ){
-                            $event = "$event AND " . $row["name"];
+                            $event = "$event " . $row["name"];
                             }
                           }
                         }
@@ -162,31 +163,31 @@ error_reporting(E_ERROR);
                     <?php
                     if ($j == 0) {
                     ?>
-                          <td class="column100 column2" data-column="column2"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column2"><?php echo ($event) ?></td>
                     <?php 
                     }elseif ($j == 1){
                     ?>
-                          <td class="column100 column3" data-column="column3"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column3"><?php echo ($event) ?></td>
                     <?php
                     }elseif ($j == 2){
                     ?>
-                          <td class="column100 column4" data-column="column4"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column4"><?php echo ($event) ?></td>
                     <?php
                     }elseif ($j == 3){
                     ?>
-                          <td class="column100 column5" data-column="column5"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column5"><?php echo ($event) ?></td>
                     <?php
                     }elseif ($j == 4){
                     ?>
-                          <td class="column100 column6" data-column="column6"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column6"><?php echo ($event) ?></td>
                     <?php
                     }elseif ($j == 5){
                     ?>
-                          <td class="column100 column7" data-column="column7"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column7"><?php echo ($event) ?></td>
                     <?php
                     }elseif ($j == 6){
                     ?>
-                          <td class="column100 column8" data-column="column8"><?php echo ($event) ?></td>
+                          <td class=<?php echo $colour ?> data-column="column8"><?php echo ($event) ?></td>
                     <?php } ?>
 
                 <?php } ?>
