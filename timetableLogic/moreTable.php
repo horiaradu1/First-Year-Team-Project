d7736d6a1f4f7b579d0f921524ed0fb0e5a35c08
k55592hr <horia.radu@student.manchester.ac.uk>
2020-03-13 18:48:32 +0000

    testing

--- timetableLogic/moreTable.php
+++ timetableLogic/moreTable.php
@@ -100,8 +100,8 @@ error_reporting(E_ERROR);
                 
                 
 
-                //$monday = date('Y-m-d',time()+( 1 - date('w'))*24*3600);
-                $monday = date('2020-03-09 00:00:00');
+                $monday = date('Y-m-d 00:00:00',time()+( 1 - date('w'))*24*3600);
+                //$monday = date('2020-03-09 00:00:00');
                 for ($i = 0; $i < 24; $i++) { 
                      $m = $i+1; 
                      ?>
