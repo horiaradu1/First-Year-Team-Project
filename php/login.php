6699302ad190d0300b7d1c2dea5b032604caa819
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-15 22:59:51 +0000

    removing nav bar from login page

--- php/login.php
+++ php/login.php
@@ -76,7 +76,7 @@
 	   }
 	?>
 	<div class="limiter">
-		<div class="navbar">
+		<!-- <div class="navbar">
 			<div class = "picture">
 			<a href="https://web.cs.manchester.ac.uk/g34904ps/team/HTML/moreTable.php">Home</a>
 		</div>
@@ -92,7 +92,7 @@
 		<div class="text100">
 			<a href="https://web.cs.manchester.ac.uk/g34904ps/team/HTML/AboutUs.php">About Us</a>
 		</div>
-		</div>
+		</div> -->
 		<div class="container-login100">
 
 			<div class="logo" >
