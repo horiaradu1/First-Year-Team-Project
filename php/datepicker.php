772a05408c289d037d56868300dad450fd329644
g34904ps <patryk.sipowicz@student.manchester.ac.uk>
2020-03-16 17:29:33 +0000

    time input box

--- php/datepicker.php
+++ php/datepicker.php
@@ -3,21 +3,35 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Datepicker - Default functionality</title>
+  <title>Create event</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
   <script>
   $( function() {
     $( "#datepicker" ).datepicker();
   } );
   </script>
+
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+</script>
+
 </head>
 <body>
 
-<p>Date: <input type="text" id="datepicker"></p>
+<p>Start: <input type="text" id="datepicker"></p>
 
+<form action="welcome.php">
+  <label for="duration">Duration in hours:</label>
+  <input type="text" id="duration" name="duration"><br><br>
+  <input type="submit" value="Submit">
+</form>
 
 </body>
 </html>
