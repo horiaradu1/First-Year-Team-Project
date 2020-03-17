82142d8a5acacbbabc6f3757a744289f120ca6ac
g34904ps <patryk.sipowicz@student.manchester.ac.uk>
2020-03-17 11:34:47 +0000

    time2

--- HTML/timepicker.php
+++ HTML/timepicker.php
@@ -8,20 +8,11 @@
 <!-- jQuery timepicker library -->
 <link rel="stylesheet" href="jquery-timepicker/jquery.timepicker.min.css">
 <script src="jquery-timepicker/jquery.timepicker.min.js"></script>
-
-
-  <script>
-  $(document).ready(function(){
-      $('#startTime').timepicker();
-  });
-  </script>
-
-
-
-
 </head>
 <body>
-
+<script>
+$(function() { $('#startTime').timepicker(); });
+</script>
 <p>Start time: <input type="text" id="startTime"/></p>
 
 <p>End time: <input type="text" id="startTime"/></p>
