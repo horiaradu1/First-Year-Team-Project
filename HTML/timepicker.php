ce7e0da37f6461e437ba8636dd19154c3c4870a5
g34904ps <patryk.sipowicz@student.manchester.ac.uk>
2020-03-17 11:45:29 +0000

    min

--- HTML/timepicker.php
+++ HTML/timepicker.php
@@ -6,7 +6,7 @@
   	<title>Timepicker for jQuery – Demos and Documentation</title>
   	<meta name="description" content="A lightweight, customizable jQuery timepicker plugin inspired by Google Calendar. Add a user-friendly javascript timepicker dropdown to your app in minutes.">
   	<script async="" src="https://www.google-analytics.com/analytics.js"></script><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  	<script type="text/javascript" src="jquery-timepicker/jquery.timepicker.js"></script>
+  	<script type="text/javascript" src="jquery-timepicker/jquery.timepicker.min.js"></script>
   	<link rel="stylesheet" type="text/css" href="jquery-timepicker/jquery.timepicker.css">
 
   	<!-- <script type="text/javascript" src="lib/bootstrap-datepicker.js"></script>
