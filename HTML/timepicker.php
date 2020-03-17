9d6b4e207293dcaaff7343d04feffa59b0ba9eef
g34904ps <patryk.sipowicz@student.manchester.ac.uk>
2020-03-17 12:18:11 +0000

    resources

--- HTML/timepicker.php
+++ HTML/timepicker.php
@@ -10,6 +10,13 @@
   	<link rel="stylesheet" type="text/css" href="jquery-timepicker/jquery.timepicker.css">
 
 
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
 </head>
 <body>
 
@@ -81,6 +88,6 @@
                       var dateOnlyExampleEl = document.getElementById('dateOnlyExample');
                       var dateOnlyDatepair = new Datepair(dateOnlyExampleEl);
                   </script>
-              
+
   	</body>
 </html>
