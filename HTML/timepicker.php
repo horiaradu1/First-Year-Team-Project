df072cbedffd06c8ebd28ae43e54ca040483090b
g34904ps <patryk.sipowicz@student.manchester.ac.uk>
2020-03-17 11:40:01 +0000

    time3

--- HTML/timepicker.php
+++ HTML/timepicker.php
@@ -1,20 +1,25 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Create event2</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!-- jQuery timepicker library -->
-<link rel="stylesheet" href="jquery-timepicker/jquery.timepicker.min.css">
-<script src="jquery-timepicker/jquery.timepicker.min.js"></script>
+
+  	<meta charset="utf-8">
+  	<title>Timepicker for jQuery </title>
+  	<meta name="description" content="A lightweight, customizable jQuery timepicker plugin inspired by Google Calendar.">
+  	<script async="" src="https://www.google-analytics.com/analytics.js"></script><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  	<script type="text/javascript" src="jquery.timepicker.js"></script>
+  	<link rel="stylesheet" type="text/css" href="jquery.timepicker.css">
+  	<script type="text/javascript" src="lib/bootstrap-datepicker.js"></script>
+  	<link rel="stylesheet" type="text/css" href="lib/bootstrap-datepicker.css">
+  	<script type="text/javascript" src="lib/site.js"></script>
+  	<link rel="stylesheet" type="text/css" href="lib/site.css">
+
 </head>
 <body>
+
+<p>Start time: <input class="time ui-timepicker-input" autocomplete = "off" type="text" id="startTime"/></p>
 <script>
 $(function() { $('#startTime').timepicker(); });
 </script>
-<p>Start time: <input type="text" id="startTime"/></p>
-
 <p>End time: <input type="text" id="startTime"/></p>
 
 <form action="welcome.php">
