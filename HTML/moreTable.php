cf311c70c839a9bfe0dafb6384a0977189b7ed0a
g34904ps <patryk.sipowicz@student.manchester.ac.uk>
2020-03-17 14:16:24 +0000

    link typo

--- HTML/moreTable.php
+++ HTML/moreTable.php
@@ -60,7 +60,7 @@ include("session.php"); ?>
       </div>
       <div class="btn-container" align="left">
 				<div class="btn1">
-				<button <button onclick="window.location.href = '/g34904ps/team/HTML/datepicker.php';"class="btn">CREATE EVENT</button>
+				<button <button onclick="window.location.href = '/g34904ps/team/HTML/addEvent.php';"class="btn">CREATE EVENT</button>
 			</div>
 			<div class="btn2">
 				<button onclick="window.location.href = '/g34904ps/team/HTML/meet.php';" class="btn">CREATE MEETING</button>
