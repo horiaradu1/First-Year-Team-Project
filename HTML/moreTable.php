5b1020ad19fd65123871bed357a8950fac57482d
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-17 03:41:11 +0000

    testing inbox

--- HTML/moreTable.php
+++ HTML/moreTable.php
@@ -33,6 +33,9 @@ include("session.php"); ?>
     <div class="text100">
       <a href="AboutUs.php">About Us</a>
     </div>
+    <div class="text100">
+      <a href="invites.php">Inbox</a>
+    </div>
     </div>
     <div class="container-table100">
       <div class="week">
