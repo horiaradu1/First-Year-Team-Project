6b2026f32b213f23bd15453b075acce45df8e4eb
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-17 01:07:35 +0000

    meeting planner tweaking

--- HTML/timetablePHP.php
+++ HTML/timetablePHP.php
@@ -306,14 +306,13 @@ $styleBoarder = "border-block-color: white; border-block-style: solid; border-ri
             $percentOccupied = ($planList[$j][$i]/sizeof($items)) * 100;
             if ($percentOccupied == 0) {
                 ?>
-                <form value="post">
+                <form action="/g63968ef/deploymenttest/visualPlanner/blank.php?day=<?php echo $j;?>&hour=<?php echo $i;?>&title=<?php echo $title;?>" style="color: black;" method="post">
                 <input type='hidden' name='items' value="<?php echo htmlentities(serialize($items));?>" />
-                <a href="/g63968ef/deploymenttest/visualPlanner/blank.php?day=<?php echo $j;?>&hour=<?php echo $i;?>&title=<?php echo $title;?>" style="color: black;">
                 <div>
                    <div style="<?php echo $styleBoarder ?>" class="accent-free"> <font color="black"><h5>All available</h5></font></div>
                  </div>
-            </a>
             </form>
+            
                 <?php }
             else if ((0 < $percentOccupied) &($percentOccupied <= 10)) {
             ?>
