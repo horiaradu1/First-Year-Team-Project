fc9e648b7c0a69f2b1d1eddae38f4f2a64c7f702
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-18 19:37:45 +0000

    coloring timetable

--- HTML/moreTable.php
+++ HTML/moreTable.php
@@ -249,7 +249,6 @@ include("session.php"); ?>
                             }
                             $colorIndex = $ids%5;
                             $listOfColors = array("gray", "lightblue", "lightgreen", "lightsalmon", "gold");
-                            echo $listOfColors[$colorIndex];
                             $classStyle = "column100 " . $listOfColors[$colorIndex];//CHANGE COLOR OF ARBITRARY EVENT IF YOU WANT
                             }
                           }
