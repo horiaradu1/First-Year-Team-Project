319fdf132edd0fc741429a090f02a5a587dbbd6f
hoimingkenny <chenghoiming@gmail.com>
2020-03-20 05:59:31 +0800

    0559

--- Kenny_Test/20_3/moreTable.php
+++ Kenny_Test/20_3/moreTable.php
@@ -1,3 +1,93 @@
+<?php
+include("session.php");
+
+// responsible for adding the courses from course selector
+if (isset($_POST["submit"])) {
+
+  // $course = mysqli_real_escape_string($db, $_POST["new_course"]);
+
+  $course = $_POST["new_course"];
+  $lab = $_POST["new_lab"];
+
+  // echo $course;
+  //
+  // echo $lab;
+
+
+  // $lab = mysqli_real_escape_string($db, $_POST["new_lab"]);
+
+  //var_dump($_POST["new_course"]);
+  $sqlAddCourse = "INSERT INTO HasCourse (username, course, lab)
+    VALUES (\"$login_session\", \"$course\", \"$lab\");";
+
+    // echo $sqlAddCourse;
+
+    $db->query($sqlAddCourse);
+    // echo $db->error;
+}
+
+
+
+// ----------------TIMETABLE INFORMATION: ------------------
+
+
+// ---------------ALL EVENTS AND COURSES----------------
+// This part of code requests all events
+$sqlGetEvents = "SELECT * FROM Events
+    WHERE eventID in
+  (SELECT eventID FROM HasEvent WHERE username = \"$login_session\")";
+
+// saving the result of the query (actual information) into a variable
+$resultEvents = mysqli_query($db, $sqlGetEvents);
+
+// Selecting all COURSES.
+$sqlGetCoursesEvents="SELECT * FROM CourseEvents
+  WHERE name in
+  (SELECT course FROM HasCourse WHERE username = \"$login_session\")";
+
+  // saving the result of that query to a variable
+$resultCoursesEvents = mysqli_query($db, $sqlGetCoursesEvents);
+
+// creating array data types in php.
+$data_array = array();
+
+while($row = mysqli_fetch_array($resultEvents, MYSQLI_ASSOC)){
+  array_push($data_array, $row);
+}
+while($x = mysqli_fetch_array($resultCoursesEvents, MYSQLI_ASSOC)){
+  array_push($data_array, $x);
+}
+
+// ----------------COURSES------------------
+// Selecting all different courses, save the result to an array (like above)
+$sqlGetCourses = "SELECT DISTINCT name  FROM CourseEvents";
+$resultCourses = mysqli_query($db, $sqlGetCourses);
+$courses_array = array();
+
+while($c = mysqli_fetch_array($resultCourses, MYSQLI_ASSOC)){
+  array_push($courses_array, $c);
+}
+
+ // if you want to check the prevwie of the array in a nicely formated way
+ // echo '<pre>';
+ // print_r($courses_array);
+ // echo '</pre>';
+
+
+ // ----------------LABS------------------
+  // Like above, but for LABS not courses.
+ $sqlGetLab = "SELECT DISTINCT lab  FROM CourseEvents";
+ $resultLab = mysqli_query($db, $sqlGetLab);
+ $lab_array = array();
+
+ while($c = mysqli_fetch_array($resultLab, MYSQLI_ASSOC)){
+   array_push($lab_array, $c);
+ }
+
+// ------------ END OF PHP ----------------
+?>
+
+
 <!--All of the HTML and css files were created using templates from colorlib, namely:
     Login Form v2 - https://colorlib.com/wp/template/login-form-v2/
     Contact Form v9 - https://colorlib.com/wp/template/contact-form-v9/-->
@@ -103,8 +193,6 @@ $fetchedInvite = $conn->query($sqlQuery);
 
       <div id="popup1" class="overlay">
         <div class="popup form" style= "height: 460px;">
-">
-
           <!-- input from the selection boxes below, sent by POST -->
           <!-- selection box for all courses -->
           <h1><b>Course Selector</b></h1>
