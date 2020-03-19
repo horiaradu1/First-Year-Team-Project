ccddad51c7ffc20d42082a18adbb5ff5afdfbec6
hoimingkenny <chenghoiming@gmail.com>
2020-03-19 02:29:54 +0000

    0229

--- Kenny_Test/addCourses.php
+++ Kenny_Test/addCourses.php
@@ -135,14 +135,7 @@ while($c = mysqli_fetch_array($resultCourses, MYSQLI_ASSOC)){
   </div>
 
   <div class="login-page">
-    <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
+    <h><b>Course Selector</b></h>
     <div>
       <!-- input from the selection boxes below, sent by POST -->
       <form method=POST>
