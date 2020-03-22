f1ff60b9e7c40bfb94091a1df484ffc2cf24c409
hoimingkenny <chenghoiming@gmail.com>
2020-03-23 00:48:11 +0800

    00

--- Kenny_Build/Mainpage.php
+++ Kenny_Build/Mainpage.php
@@ -225,8 +225,8 @@
       <div class="col-1">123</div>
       <div class="col-10 justify-content-between align-items-center">
 
-        <button class="col-6 btn btn-info btn-lg">CREATE COURSE</button>
-        <button class="col-6 btn btn-info btn-lg">CREATE EVENT</button>
+        <button class="col-5 btn btn-info btn-lg">CREATE COURSE</button>
+        <button class="col-5 btn btn-info btn-lg">CREATE EVENT</button>
  
       </div>
       <div class="col-1">123</div>
