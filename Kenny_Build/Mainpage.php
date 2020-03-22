83d7164c1e71d6f73612330ac19a8c02e0e533be
hoimingkenny <chenghoiming@gmail.com>
2020-03-23 00:49:43 +0800

    00

--- Kenny_Build/Mainpage.php
+++ Kenny_Build/Mainpage.php
@@ -222,7 +222,7 @@
       ?></a>
     </h1>
     <div class="row">
-      <div class="col-1">123</div>
+      <div class="col-1 col-offset-1">123</div>
       <div class="col-8 justify-content-between align-items-center">
 
         <button class="col-5 btn btn-info btn-lg">CREATE COURSE</button>
