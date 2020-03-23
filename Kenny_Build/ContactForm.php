8b49def5c5b6f0372ef2963280ddde562040a2a9
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 02:30:27 +0000

    fix

--- Kenny_Build/ContactForm.php
+++ Kenny_Build/ContactForm.php
@@ -5,7 +5,18 @@
 	<meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel = "stylesheet" type = "text/css" href = "contactForm.css">
-	<link rel = "icon" type = "image/x-icon" href = "https://images.gr-assets.com/users/1582104594p8/110300593.jpg">
+  <link rel = "icon" type = "image/x-icon" href = "https://images.gr-assets.com/users/1582104594p8/110300593.jpg">
+        <link rel = "stylesheet" type = "text/css" href = "meet.css">
+        <link rel = "icon" href =
+"https://images.gr-assets.com/users/1582104594p8/110300593.jpg"
+        type = "image/x-icon">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <link rel="stylesheet" href="mainpage.css">
+  <link rel = "icon" href="https://images.gr-assets.com/users/1582104594p8/110300593.jpg" type = "image/x-icon">
+	<!-- <link rel = "icon" type = "image/png" href = "Logo.png"> -->
 </head>
 <?php
 include("session.php"); 
@@ -20,30 +31,44 @@ $fetchedInvite = $conn->query($sqlQuery);
 ?>
 
 <body>
-  <div class="container-form">
-		<div class="navbar">
-			<div class = "picture">
-			<a href="moreTable.php">Home</a>
-		</div>
-		<div class = "picture">
-			<a href="meet.php">Meeting</a>
-		</div>
-	<div class="text100">
-			<a href="logout.php">Sign out</a>
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" style="margin-right: 0rem;" href="moreTable.php"><i class="far fa-clock"></i> TimeOnTable   </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <?php 
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      $sqlQuery = "SELECT eventID FROM Inbox WHERE username = " . "'" . ($login_session) . "'";
+      $fetchedInvite = $conn->query($sqlQuery);
+      ?>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="meet.php">Meeting Planner<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+        
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Welcome, <?php echo($login_session) ?>!</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="invites.php">Inbox(<?php if ($fetchedInvite->num_rows < 10) {echo $fetchedInvite->num_rows;} else {echo "+9";} ?>)</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="AboutUs.php">About Us</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="ContactForm.php">Contact</i></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="logout.php">Sign Out</i></a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="text100">
-        <a><?php echo($login_session) ?></a>
-    </div>
-		<div class="text100">
-			<a href="ContactForm.php">Contact Us</a>
-		</div>
-		<div class="text100">
-			<a href="AboutUs.php">About Us</a>
-		</div>
-    <div class="text100">
-      <a href="invites.php">Inbox(<?php if ($fetchedInvite->num_rows < 10) {echo $fetchedInvite->num_rows;} else {echo "+9";} ?>)</a>
-    </div>
-		</div>
+  </nav>
 		<div class="logo" >
 			<img src = "Logo.png">
 		</div>
