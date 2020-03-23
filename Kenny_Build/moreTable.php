146f8888f71099037d9786c9179dcda6c7c11a3c
hoimingkenny <chenghoiming@gmail.com>
2020-03-23 09:26:56 +0800

    0858

--- Kenny_Build/moreTable.php
+++ Kenny_Build/moreTable.php
@@ -150,9 +150,9 @@
   <link rel = "stylesheet" type = "text/css" href = "meet.css">
   <link rel="stylesheet" href="css/bootstrap.css">
 
-  <link rel="stylesheet" href="/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/css/fontawesome_css/font-awesome.min.css">
-  <link rel="stylesheet" href="/css/fontawesome_css/all.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <link rel="stylesheet" href="mainpage.css">
   <link rel = "icon" href="https://images.gr-assets.com/users/1582104594p8/110300593.jpg" type = "image/x-icon">
   <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
