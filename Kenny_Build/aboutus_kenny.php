4ef29d44de5af93de1acdd2148d760a799117e0f
hoimingkenny <chenghoiming@gmail.com>
2020-03-23 10:48:36 +0800

    0858

--- Kenny_Build/aboutus_kenny.php
+++ Kenny_Build/aboutus_kenny.php
@@ -74,22 +74,22 @@
   </div>
 
   <div class="row">
-    <div class="col-3">
+    <div class="col-lg-3">
       <div class="thumbnail">
         <img src="https://source.unsplash.com/ZMZHcvIVgbg/300x300">
       </div>
     </div>
-    <div class="col-3">
+    <div class="col-lg-3">
       <div class="thumbnail">
         <img src="https://source.unsplash.com/ZMZHcvIVgbg/300x300">
       </div>
     </div>
-    <div class="col-3">
+    <div class="col-lg-3">
       <div class="thumbnail">
         <img src="https://source.unsplash.com/ZMZHcvIVgbg/300x300">
       </div>
     </div>
-    <div class="col-3">
+    <div class="col-lg-3">
       <div class="thumbnail">
         <img src="https://source.unsplash.com/ZMZHcvIVgbg/300x300">
       </div>
