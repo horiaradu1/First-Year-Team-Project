71598f3c68f2c5e4a5ed9ac6e81cde7f0c4c88b2
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 01:40:04 +0000

    buttons work

--- Kenny_Build/moreTable.php
+++ Kenny_Build/moreTable.php
@@ -184,7 +184,7 @@
 <body>
   <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="moreTable.php"><i class="far fa-clock"></i> TimeOnTable   </a>
+      <a class="navbar-brand" style="margin-right: 0rem;" href="moreTable.php"><i class="far fa-clock"></i> TimeOnTable   </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
