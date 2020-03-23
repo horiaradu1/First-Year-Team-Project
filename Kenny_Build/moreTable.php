c4e536fabc4f757791dc724e5b3ba696532e3ca4
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 00:50:12 +0000

    refurbishing about us page

--- Kenny_Build/moreTable.php
+++ Kenny_Build/moreTable.php
@@ -1,4 +1,5 @@
 <?php
+  error_reporting(E_ERROR);
   include("session.php");
   // responsible for adding the courses from course selector
   if (isset($_POST["submit"])) {
@@ -80,7 +81,7 @@
 <!--All of the HTML and css files were created using templates from colorlib, namely:
     Login Form v2 - https://colorlib.com/wp/template/login-form-v2/
     Contact Form v9 - https://colorlib.com/wp/template/contact-form-v9/-->
-<?php error_reporting(E_ERROR); ?>
+
 
 <!--  ------------------ADDEVENT.php -->
 <?php
