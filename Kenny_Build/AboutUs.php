621f32c23e6910f6bfdaa3ae8036d8072852ff27
hoimingkenny <chenghoiming@gmail.com>
2020-03-23 09:28:12 +0800

    0858

--- Kenny_Build/AboutUs.php
+++ Kenny_Build/AboutUs.php
@@ -20,9 +20,9 @@
 	<meta charset = "utf-8" />
     <!-- add icon link -->
     <link rel = "icon" href ="https://images.gr-assets.com/users/1582104594p8/110300593.jpg" type = "image/x-icon">]
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/fontawesome_css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <link rel="stylesheet" href="mainpage.css">
     <link rel = "icon" href="https://images.gr-assets.com/users/1582104594p8/110300593.jpg" type = "image/x-icon">
     <link rel = "stylesheet" type = "text/css" href = "AboutUs.css">
