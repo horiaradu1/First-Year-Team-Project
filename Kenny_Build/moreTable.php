a2da6982fe8c3fcfa29b1e1535d00d613522fa75
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 01:14:39 +0000

    refurbishing about us page

--- Kenny_Build/moreTable.php
+++ Kenny_Build/moreTable.php
@@ -200,7 +200,7 @@
             <a class="nav-link" href="#">Welcome, <?php echo($login_session) ?>!</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="invites.php">Inbox(<?php if ($fetchedInvite->num_rows < 10) {echo $fetchedInvite->num_rows;} else {echo "+9";} ?>)</a>
+            <a class="nav-link" style="padding: 0px;" href="invites.php">Inbox(<?php if ($fetchedInvite->num_rows < 10) {echo $fetchedInvite->num_rows;} else {echo "+9";} ?>)</a>
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="AboutUs.php">About Us</a>
