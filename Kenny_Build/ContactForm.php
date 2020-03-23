85a281c8af5c33a79c17e8e46a508a2dbe97a801
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 03:00:09 +0000

    contact form

--- Kenny_Build/ContactForm.php
+++ Kenny_Build/ContactForm.php
@@ -14,7 +14,6 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-  <link rel="stylesheet" href="mainpage.css">
   <link rel = "icon" href="https://images.gr-assets.com/users/1582104594p8/110300593.jpg" type = "image/x-icon">
 	<!-- <link rel = "icon" type = "image/png" href = "Logo.png"> -->
 </head>
