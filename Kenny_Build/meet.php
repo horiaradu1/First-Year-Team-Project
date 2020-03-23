80c70a7979313c1e536003219b991a235fe989b0
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 00:20:06 +0000

    refurbishing about us page

--- Kenny_Build/meet.php
+++ Kenny_Build/meet.php
@@ -95,7 +95,7 @@ if('POST' === $_SERVER['REQUEST_METHOD']) {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="Mainpage.html">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="moreTable.php">Home<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="meet.php">Meeting Planner<span class="sr-only">(current)</span></a>
