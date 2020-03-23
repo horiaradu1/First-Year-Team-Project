c551b811b7ad7b39a1538dbffcc996d2458da224
hoimingkenny <chenghoiming@gmail.com>
2020-03-23 10:40:34 +0800

    0858

--- Kenny_Build/aboutus_kenny.php
+++ Kenny_Build/aboutus_kenny.php
@@ -41,9 +41,6 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-          <a class="nav-link" href="moreTable.php">Home<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
             <a class="nav-link" href="meet.php">Meeting Planner<span class="sr-only">(current)</span></a>
           </li>
         </ul>
