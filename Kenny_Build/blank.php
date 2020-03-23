862eb7c1366d807cd3267188d7e09b1ae80281bc
Eirik Fladmark <eirik.fladmark@gmail.com>
2020-03-23 04:22:27 +0000

    contact fix

--- Kenny_Build/blank.php
+++ Kenny_Build/blank.php
@@ -143,32 +143,44 @@ $fetchedInvite = $conn->query($sqlQuery);
 </head>
 <body>
     
-<div class="navbar">
-      <div class = "picture">
-      <a href="moreTable.php">Home</a>
-       <!-- connect to the login -->
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" style="margin-right: 0rem;" href="moreTable.php"><i class="far fa-clock"></i> TimeOnTable   </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <?php 
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      $sqlQuery = "SELECT eventID FROM Inbox WHERE username = " . "'" . ($login_session) . "'";
+      $fetchedInvite = $conn->query($sqlQuery);
+      ?>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="meet.php">Meeting Planner<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+        
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Welcome, <?php echo($login_session) ?>!</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="invites.php">Inbox(<?php if ($fetchedInvite->num_rows < 10) {echo $fetchedInvite->num_rows;} else {echo "+9";} ?>)</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="AboutUs.php">About Us</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="ContactForm.php">Contact</i></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="logout.php">Sign Out</i></a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class = "picture">
-      <a href="meet.php">Meeting</a>
-       <!-- connect to the login -->
-    </div>
-  <div class="text100">
-      <a href="logout.php">Sign out</a>
-       <!-- connect to the login -->
-    </div>
-    <div class="text100">
-        <a><?php echo($login_session) ?></a>
-    </div>
-    <div class="text100">
-      <a href="ContactForm.php">Contact Us</a>
-    </div>
-    <div class="text100">
-      <a href="AboutUs.php">About Us</a>
-    </div>
-    <div class="text100">
-      <a href="invites.php">Inbox(<?php if ($fetchedInvite->num_rows < 10) {echo $fetchedInvite->num_rows;} else {echo "+9";} ?>)</a>
-    </div>
-    </div>
+  </nav>
 
 <div class="container-login100">
 			<div class="wrap-register">
